d7aff716a0c08ae62e706429a4317851035af702
Valeria Khristinina <valeria.khristinina@gmail.com>
2020-03-01 18:20:25 +0100

    Initial Commit

---
+++ HW2.10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.10
+//
+//  Created by Valeria Khristinina on 01/03/2020.
+//  Copyright © 2020 Valeria Khristinina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
